d7d43e160e0c10ee13421578ced51853492cfbc5
amineelgardoum-rgb <amine.elgardoum@etu.uae.ac.ma>
2026-02-14 10:48:34 +0100

    Rename table from erp_cut_az12 to erp_cust_az12

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -69,10 +69,10 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
 	maintenance NVARCHAR(50)
 )
 
-IF OBJECT_ID ('bronze.erp_cut_az12','U') IS NOT NULL 
-	DROP TABLE bronze.erp_cut_az12;
+IF OBJECT_ID ('bronze.erp_cust_az12','U') IS NOT NULL 
+	DROP TABLE bronze.erp_cust_az12;
 
-CREATE TABLE bronze.erp_cut_az12 (
+CREATE TABLE bronze.erp_cust_az12 (
 	cid NVARCHAR(50),
 	dbate DATE,
 	gen NVARCHAR (50)
